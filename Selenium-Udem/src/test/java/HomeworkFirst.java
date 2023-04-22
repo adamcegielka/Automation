89b048709ba43c6d15885f9de94b853ff14e82c5
Adam <adamcegielka@tlen.pl>
2023-04-22 10:49:19 +0200

    🔧 HomeworkFirst: WebDriver with Chrome, Firefox, IE browsers

---
+++ Selenium-Udem/src/test/java/HomeworkFirst.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class HomeworkFirst {
+
+    @Test
+    public void openGooglePage() {
+        WebDriver driver = getDriver("chrome");
+    }
+
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                return new ChromeDriver();
+            case "firefox":
+                return new FirefoxDriver();
+            case "ie":
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+
+}

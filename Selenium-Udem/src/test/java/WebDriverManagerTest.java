19738ec695bea06460194fa7c3c6317171c6f194
Adam <adamcegielka@tlen.pl>
2023-04-22 12:12:13 +0200

    ➕ addition of WebDriverManager

---
+++ Selenium-Udem/src/test/java/WebDriverManagerTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebDriverManagerTest {
+/*
+    @Test
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+    }
+
+
+    <dependency>
+    <groupId>io.github.bonigarcia</groupId>
+    <artifactId>webdrivermanager</artifactId>
+    <version>5.3.2</version>
+    <scope>test</scope>
+    </dependency>
+*/
+
+}

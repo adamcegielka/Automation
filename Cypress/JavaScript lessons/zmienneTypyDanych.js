a173d0e08eeb0763af599c53352975e8fced179e
Adam <adamcegielka@tlen.pl>
2023-04-26 17:10:31 +0200

    ➕ addition of js lessons

---
+++ Cypress/JavaScript lessons/zmienneTypyDanych.js
@@ -0,0 +1,39 @@
+console.log("Lessons of JavaScript")
+
+let x = 4;
+let a = "Words";
+x = 9;
+a = "New words";
+
+console.log(x + ' ' + a);
+
+
+const myName = "Adam";
+console.log(myName);
+
+
+let y = 102;
+let b = "Hello World!";
+let c = true;
+console.log(typeof(y + ' ' + b + ' ' + c));
+
+
+const undef = undefined;
+const nuull = null;
+console.log(undef, nuull);
+
+
+const board = [3, "Eva", 5, "Adam"];
+console.log(board);
+console.log(board[1]);
+
+
+const days = {
+    week: "Tuesday",
+    month: "May",
+    year: 2023
+}
+console.log(days);
+
+days.month = "June"
+console.log(days.month);

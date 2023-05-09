6d9ac3bf9582bd03fdc78e8ea984060d1aa63221
Adam <adamcegielka@tlen.pl>
2023-05-09 17:17:09 +0200

    🔧 Update example.spec.ts

--- Playwright-Automated-Software-Testing/tests/example.spec.ts
+++ Playwright-Automated-Software-Testing/tests/example.spec.ts
@@ -1,11 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('First test', async ({ page }) => {
-  const url = 'https://automationteststore.com/';
-  await page.goto(url);
-  await expect(page).toHaveURL(url);
-});
-
 test('Simple basic test', async ({ page }) => {
   await page.goto('https://www.example.com/');
   const pageTitle = await page.locator('h1');

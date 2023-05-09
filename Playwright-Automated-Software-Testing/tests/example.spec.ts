5eeaad46a74614ced2f78dbfcf270cbf0ab1128a
Adam <adamcegielka@tlen.pl>
2023-05-09 18:03:58 +0200

    🔧 Update examples.spec.ts

--- Playwright-Automated-Software-Testing/tests/example.spec.ts
+++ Playwright-Automated-Software-Testing/tests/example.spec.ts
@@ -5,3 +5,15 @@ test('Simple basic test', async ({ page }) => {
   const pageTitle = await page.locator('h1');
   await expect(pageTitle).toContainText('Example Domain');
 });
+
+test.only('Clickin on elements', async ({ page }) => {
+
+  const url = 'http://zero.webappsecurity.com/';  
+  await page.goto(url);
+  await page.click('#signin_button');
+  await page.click('text=Sign in');
+
+  const errorMessage = await page.locator('.alert-error');  
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');
+  
+});

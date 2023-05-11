c9bfa912d4e86ec2ac39a1c9663c5a59e285179d
Adam <adamcegielka@tlen.pl>
2023-05-11 15:40:01 +0200

    🚀 adding Lesson_12

---
+++ Playwright/Lesson_12 - S0304 Page Object Model in negative tests/tests/desktop.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.date';
+import { LoginPage } from '../pages/login.page';
+
+test.describe('Desktop testing', () => {
+  test.beforeEach(async ({ page }) => {
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+
+    await page.goto('/');
+    // Page Object Model - POM
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
+  });
+
+  test('quick payment with correct data', async ({ page }) => {
+    // Arrang
+    const receiverId = '2';
+    const transferAmount = '150';
+    const transferTitle = 'Zwrot kasy';
+    const expectedTransferReceiver = 'Chuck Demobankowy';
+
+    // Act
+    await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+    await page.locator('#widget_1_transfer_title').fill(transferTitle);
+    await page.getByRole('button', { name: 'wykonaj' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(
+      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
+    );
+  });
+
+  test('successful mobile phone top-up', async ({ page }) => {
+    // Arrang
+    const phoneNumber = '502 xxx xxx';
+    const topUpAmount = '90';
+    const expectMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${phoneNumber}`;
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(expectMessage);
+  });
+
+  test('correct balance after successful mobile phone top-up', async ({
+    page,
+  }) => {
+    // Arrang
+    const phoneNumber = '502 xxx xxx';
+    const topUpAmount = '90';
+    const expectMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${phoneNumber}`;
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectBalance = Number(initialBalance) - Number(topUpAmount);
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectBalance}`);
+  });
+});
---
+++ Playwright/Lesson_12 - S0304 Page Object Model in negative tests/tests/login.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.date';
+import { LoginPage } from '../pages/login.page';
+
+test.describe('User login to Demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('successful login with too short username', async ({ page }) => {
+    // Arrang
+
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act // Page Object Model - POM
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
+
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrang
+    const shortUserId = 'testy';
+    const expectErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(shortUserId);
+    await page.getByTestId('password-input').click();
+
+    // Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectErrorMessage
+    );
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrang
+    const userId = loginData.userId;
+    const userPassword = '87654';
+    const expectErrorMessage = 'hasło ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('password-input').blur();
+
+    // Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectErrorMessage
+    );
+  });
+});

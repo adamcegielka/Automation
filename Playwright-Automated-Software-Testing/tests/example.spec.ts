9e54bd13db5ddd56a8cfb2d9747283291e694745
Adam <adamcegielka@tlen.pl>
2023-05-14 19:03:33 +0200

    🔧 Adding new test: working with inputs

--- Playwright-Automated-Software-Testing/tests/example.spec.ts
+++ Playwright-Automated-Software-Testing/tests/example.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('Simple basic test', async ({ page }) => {
+test('simple basic test', async ({ page }) => {
   await page.goto('https://www.example.com/');
   const pageTitle = await page.locator('h1');
   await expect(pageTitle).toContainText('Example Domain');
 });
 
-test.only('Clickin on elements', async ({ page }) => {
+test('clickin on elements', async ({ page }) => {
 
   const url = 'http://zero.webappsecurity.com/';  
   await page.goto(url);
@@ -14,6 +14,17 @@ test.only('Clickin on elements', async ({ page }) => {
   await page.click('text=Sign in');
 
   const errorMessage = await page.locator('.alert-error');  
-  await expect(errorMessage).toContainText('Login and/or password are wrong.');
-  
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');  
+});
+
+test('working with inputs', async ({ page }) => {
+  const url = 'http://zero.webappsecurity.com/';
+  await page.goto(url);
+  await page.click('#signin_button');
+  await page.type('#user_login', 'some username');
+  await page.type('#user_password', '123456');
+  await page.click('text=Sign in');
+
+  const errorMessage = await page.locator('.alert-error');  
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');  
 });

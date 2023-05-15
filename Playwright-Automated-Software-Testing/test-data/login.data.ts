24e4ecf25ddaf887455e009ad191144d8ae229bb
Adam <adamcegielka@tlen.pl>
2023-05-15 17:28:51 +0200

    🚀 adding login.data.ts

---
+++ Playwright-Automated-Software-Testing/test-data/login.data.ts
@@ -0,0 +1,23 @@
+export const loginData = {
+  // userId  
+  userIdCorrect: 'adrianj',
+  userIdWrong: 'adr1anj',
+  userIdShort: 'adrian',
+  userIdLong: 'adrianje',
+  userIdEmpty: '',
+  userIdSpace: ' ',
+  //possword
+  posswordCorrect: 'dcS0qAgT',
+  posswordWrong: 'dcS0qAgt',
+  posswordShort: 'dcS0qA',
+  posswordLong: 'dcS0qAgT4',
+  passwordEmpty: '',
+  passwordSpace: '',
+  // email
+  emailCorrect: 'adrian.jackson@mail.com',
+  emailWrong: 'adrianjackson@mail.com',
+  emailWithoutSign: 'adrian.jacksonmail.com',
+  emailWithoutCom: 'adrian.jackson@mail',
+  emailEmpty: '',
+  emailSpace: ' '
+};

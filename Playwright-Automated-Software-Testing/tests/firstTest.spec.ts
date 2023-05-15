9edf6ed6d20c92fe0bfa1e60b2b778aac822847a
Adam <adamcegielka@tlen.pl>
2023-05-15 18:27:33 +0200

    🔧 Adding skip - ignoring the test

--- Playwright-Automated-Software-Testing/tests/firstTest.spec.ts
+++ Playwright-Automated-Software-Testing/tests/firstTest.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('First test', async ({ page }) => {
+// .skip -> ignoring the test
+test.skip('First test', async ({ page }) => {
   const url = 'https://automationteststore.com/';
   await page.goto(url);
   await expect(page).toHaveURL(url);

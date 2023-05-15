016363f7acd36184e11ce3931ee80ba62ce24928
Adam <adamcegielka@tlen.pl>
2023-05-15 19:15:30 +0200

    🔧 update code

--- Playwright-Automated-Software-Testing/tests/example.spec.ts
+++ Playwright-Automated-Software-Testing/tests/example.spec.ts
@@ -1,47 +1,59 @@
 import { test, expect } from '@playwright/test';
 
-test('simple basic test', async ({ page }) => {
+// ASSERTIONS: test.only(), test.skip(), test.fail(), test.fixme(), test.slow()
+test.skip('simple basic test', async ({ page }) => {
   await page.goto('https://www.example.com/');
   const pageTitle = await page.locator('h1');
   await expect(pageTitle).toContainText('Example Domain');
 });
 
+// CLICKIN ON ELEMENTS
 test('clickin on elements', async ({ page }) => {
-
-  const url = 'http://zero.webappsecurity.com/';  
-  await page.goto(url);
-  await page.click('#signin_button');
-  await page.click('text=Sign in');
-
-  const errorMessage = await page.locator('.alert-error');  
-  await expect(errorMessage).toContainText('Login and/or password are wrong.');  
-});
-
-test('working with inputs', async ({ page }) => {
   const url = 'http://zero.webappsecurity.com/';
   await page.goto(url);
   await page.click('#signin_button');
-  await page.type('#user_login', 'some username');
-  await page.type('#user_password', '123456');
   await page.click('text=Sign in');
 
-  const errorMessage = await page.locator('.alert-error');  
-  await expect(errorMessage).toContainText('Login and/or password are wrong.');  
+  const errorMessage = await page.locator('.alert-error');
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');
 });
 
-test.only('assertions', async ({ page }) => {
-  const url = 'https://www.example.com/';
-  await page.goto(url);
-  await expect(page).toHaveURL(url);
-  await expect(page).toHaveTitle('Example Domain');
+// MULTIPLE TESTS IN ONE
+test.describe('Test suite', () => {
 
-  // Checking a existent element
-  const element = await page.locator('h1');
-  await expect(element).toBeVisible();
-  await expect(element).toHaveText('Example Domain');
-  await expect(element).toHaveCount(1);
+  // WORKING WITH INPUTS
+  test('working with inputs', async ({ page }) => {
+    const url = 'http://zero.webappsecurity.com/';
+    await page.goto(url);
+    await page.click('#signin_button');
+    await page.type('#user_login', 'some username');
+    await page.type('#user_password', '123456');
+    await page.click('text=Sign in');
 
-  // Checking a non-existent element
-  const nonExistingElement = await page.locator('h5');
-  await expect(nonExistingElement).not.toBeVisible();
+    const errorMessage = await page.locator('.alert-error');
+    await expect(errorMessage).toContainText(
+      'Login and/or password are wrong.'
+    );
+  });
+
+  // ASSERTION AND TAG TEST - @nameTag -- npx playwright test --grep @assert / OPPOSITE: npx playwright test --grep--invert @assert
+  // https://playwright.dev/docs/test-assertions
+  // https://playwright.dev/docs/test-annotations#tag-tests
+
+  test('assertions @assert', async ({ page }) => {
+    const url = 'https://www.example.com/';
+    await page.goto(url);
+    await expect(page).toHaveURL(url);
+    await expect(page).toHaveTitle('Example Domain');
+
+    // Checking a existent element
+    const element = await page.locator('h1');
+    await expect(element).toBeVisible();
+    await expect(element).toHaveText('Example Domain');
+    await expect(element).toHaveCount(1);
+
+    // Checking a non-existent element
+    const nonExistingElement = await page.locator('h5');
+    await expect(nonExistingElement).not.toBeVisible();
+  });
 });

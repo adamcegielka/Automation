0469be7f102d8f788d5c64732a5449d01fc6f383
Adam <adamcegielka@tlen.pl>
2023-05-17 20:29:35 +0200

    🚀  Adding screenshots.spec.ts

---
+++ Playwright-Automated-Software-Testing/tests/screenshots.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+// SCREENSHOTS
+// https://playwright.dev/docs/screenshots
+test.describe('Screenshots', () => {
+
+  test('full page screenshots', async ({ page }) => {
+    const url = 'https://www.example.com/';
+    await page.goto(url);
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  });
+
+  test('element screenshot', async ({ page }) => {
+    const url = 'http://zero.webappsecurity.com/';
+    await page.goto(url);
+    await page.locator('.carousel-inner').screenshot({ path: 'scren_carusel.png' });
+  });
+
+  test.only('single element screenshot', async ({ page }) => {
+    const url = 'https://www.example.com/';
+    await page.goto(url);
+    const element = await page.$('h1');
+    await element.screenshot( { path: 'scren_single_element.png' });
+  });
+});

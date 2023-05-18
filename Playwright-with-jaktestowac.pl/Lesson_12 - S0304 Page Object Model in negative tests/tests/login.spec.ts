47d37ffdc735c0ab9fc3f57ba476f4a67b68d207
Adam <adamcegielka@tlen.pl>
2023-05-18 15:57:22 +0200

    🔧 update

--- Playwright-with-jaktestowac.pl/Lesson_12 - S0304 Page Object Model in negative tests/tests/login.spec.ts
+++ Playwright-with-jaktestowac.pl/Lesson_12 - S0304 Page Object Model in negative tests/tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { loginData } from '../test-data/login.date';
+import { loginData } from '../test-data/login.data';
 import { LoginPage } from '../test-pages/login.page';
 
 test.describe('User login to Demobank', () => {

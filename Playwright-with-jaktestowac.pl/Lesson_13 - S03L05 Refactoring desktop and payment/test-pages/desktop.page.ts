97edc45ccc31dc582dc39ccda21ef2558478f288
Adam <adamcegielka@tlen.pl>
2023-05-18 19:53:47 +0200

    🔧 adding userName for login.spec.ts

--- Playwright-with-jaktestowac.pl/Lesson_13 - S03L05 Refactoring desktop and payment/test-pages/desktop.page.ts
+++ Playwright-with-jaktestowac.pl/Lesson_13 - S03L05 Refactoring desktop and payment/test-pages/desktop.page.ts
@@ -1,20 +1,22 @@
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
 export class DesktopPage {
-    constructor(private page: Page) {}
+  constructor(private page: Page) {}
 
-    transferReceiver = this.page.locator('#widget_1_transfer_receiver');
-    transferAmount  = this.page.locator('#widget_1_transfer_amount');
-    transferTitle  = this.page.locator('#widget_1_transfer_title');
-    buttonMake  = this.page.getByRole('button', { name: 'wykonaj' });
-    buttonClose  = this.page.getByTestId('close-button');
+  transferReceiver = this.page.locator('#widget_1_transfer_receiver');
+  transferAmount = this.page.locator('#widget_1_transfer_amount');
+  transferTitle = this.page.locator('#widget_1_transfer_title');
+  buttonMake = this.page.getByRole('button', { name: 'wykonaj' });
+  buttonClose = this.page.getByTestId('close-button');
 
-    messageText  = this.page.locator('#show_messages');
+  messageText = this.page.locator('#show_messages');
 
-    topupReceiver  = this.page.locator('#widget_1_topup_receiver');
-    topupAmount  = this.page.locator('#widget_1_topup_amount');
-    topupAgreement  = this.page.locator('#uniform-widget_1_topup_agreement span');
-    topUpPhone  = this.page.getByRole('button', { name: 'doładuj telefon' });
+  topupReceiver = this.page.locator('#widget_1_topup_receiver');
+  topupAmount = this.page.locator('#widget_1_topup_amount');
+  topupAgreement = this.page.locator('#uniform-widget_1_topup_agreement span');
+  topUpPhone = this.page.getByRole('button', { name: 'doładuj telefon' });
 
-    moneyValue  = this.page.locator('#money_value');
+  moneyValue = this.page.locator('#money_value');
+
+  userName = this.page.getByTestId('user-name');
 }

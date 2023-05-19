9bbef2896e15be26e6516a1f52da989955331a51
Adam <adamcegielka@tlen.pl>
2023-05-19 13:54:45 +0200

    🔧 update screens

--- Playwright-Automated-Software-Testing/tests/screenshots.spec.ts
+++ Playwright-Automated-Software-Testing/tests/screenshots.spec.ts
@@ -7,19 +7,19 @@ test.describe('Screenshots', () => {
   test('full page screenshots', async ({ page }) => {
     const url = 'https://www.example.com/';
     await page.goto(url);
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    await page.screenshot({ path: 'screens/screenshot.png', fullPage: true });
   });
 
   test('element screenshot', async ({ page }) => {
     const url = 'http://zero.webappsecurity.com/';
     await page.goto(url);
-    await page.locator('.carousel-inner').screenshot({ path: 'scren_carusel.png' });
+    await page.locator('.carousel-inner').screenshot({ path: 'screens/scren_carusel.png' });
   });
 
   test('single element screenshot', async ({ page }) => {
     const url = 'https://www.example.com/';
     await page.goto(url);
     const element = await page.locator('h1');
-    await element.screenshot( { path: 'scren_single_element.png' });
+    await element.screenshot( { path: 'screens/scren_single_element.png' });
   });
 });

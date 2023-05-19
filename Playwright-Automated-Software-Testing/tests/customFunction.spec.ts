0fd8d0cebf2885ad220082f46821bbf1bfacda11
Adam <adamcegielka@tlen.pl>
2023-05-19 12:46:52 +0200

    🚀  Adding Custom Helpers

---
+++ Playwright-Automated-Software-Testing/tests/customFunction.spec.ts
@@ -0,0 +1,9 @@
+// Custom Function - file: helpers.ts
+import { test, expect } from '@playwright/test';
+import { assertTitle, loadHomePage } from '../helpers';
+
+// ASSERTIONS: test.only(), test.skip(), test.fail(), test.fixme(), test.slow()
+test('Custom Helpers', async ({ page }) => {
+  await loadHomePage(page);
+  await assertTitle(page);
+});

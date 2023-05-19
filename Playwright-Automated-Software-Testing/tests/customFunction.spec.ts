ee6c0bf6fd8dae917d5e945627d1daab7b6fdcc9
Adam <adamcegielka@tlen.pl>
2023-05-19 13:30:18 +0200

    🔧 update

--- Playwright-Automated-Software-Testing/tests/customFunction.spec.ts
+++ Playwright-Automated-Software-Testing/tests/customFunction.spec.ts
@@ -2,7 +2,6 @@
 import { test, expect } from '@playwright/test';
 import { assertTitle, loadHomePage } from '../helpers';
 
-// ASSERTIONS: test.only(), test.skip(), test.fail(), test.fixme(), test.slow()
 test('Custom Helpers', async ({ page }) => {
   await loadHomePage(page);
   //   await page.pause();      // use for debagging

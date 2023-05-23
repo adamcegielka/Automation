eca0d33745ae44b881484501e4cdf98ad6ff1954
Adam <adamcegielka@tlen.pl>
2023-05-23 21:08:52 +0200

    🔧 update

--- Playwright-Automated-Software-Testing/tests/e2e-data/e2e-login.data.ts
+++ Playwright-Automated-Software-Testing/tests/e2e-data/e2e-login.data.ts
@@ -1,16 +1,8 @@
 export const loginRegistrationData = {
     // CORRECT
-    userLogin: 'Leon Baran',
-    userPassword: 'amJ57ey63G',
-    userEmail: 'leon.baran@mail.com',
-    firstName: 'Leon',
-    lastName: 'Baran',
-    mobileNumber: '000111222',
+    userLogin: 'username',
+    userPassword: 'password',
     // INCORRECT
     userLoginIncorrect: 'Jan Dragon',
     userPasswordIncorrect: 'emJ57ey63g',
-    userEmailIncorrect: 'jan.dragon@mail.com',
-    firstNameIncorrect: 'Leon',
-    lastNameIncorrect: 'Dragon',
-    mobileNumberIncorrect: '000lll222'
 }

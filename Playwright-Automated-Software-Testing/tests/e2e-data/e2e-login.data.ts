b1b07251a1e16c1b635fa81a3828013314a58802
Adam <adamcegielka@tlen.pl>
2023-05-23 19:23:04 +0200

    🚀  Adding e2e-login.data.ts

---
+++ Playwright-Automated-Software-Testing/tests/e2e-data/e2e-login.data.ts
@@ -0,0 +1,16 @@
+export const loginRegistrationData = {
+    // CORRECT
+    userLogin: 'Leon Baran',
+    userPassword: 'amJ57ey63G',
+    userEmail: 'leon.baran@mail.com',
+    firstName: 'Leon',
+    lastName: 'Baran',
+    mobileNumber: '000111222',
+    // INCORRECT
+    userLoginIncorrect: 'Jan Dragon',
+    userPasswordIncorrect: 'emJ57ey63g',
+    userEmailIncorrect: 'jan.dragon@mail.com',
+    firstNameIncorrect: 'Leon',
+    lastNameIncorrect: 'Dragon',
+    mobileNumberIncorrect: '000lll222'
+}

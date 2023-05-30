1aa8517edffa0352723c97569d3bd3da7936cca5
Adam <adamcegielka@tlen.pl>
2023-05-30 15:11:33 +0200

    🔧 update

--- Playwright-Automated-Software-Testing/page-objects/LoginPage.ts
+++ Playwright-Automated-Software-Testing/page-objects/LoginPage.ts
@@ -14,11 +14,24 @@ export class LoginPage {
     this.userNameInput = page.locator('#user_login');
     this.userPasswordInput = page.locator('#user_password');
     this.submitButton = page.locator('text=Sign in');
-    this.errorMessage = page.locator('.alert-error')
+    this.errorMessage = page.locator('.alert-error');
+  }
+
+  async login(userName: string, userPassword: string) {
+    await this.userNameInput.type(userName);
+    await this.userPasswordInput.type(userPassword);
+    await this.submitButton.click();
+  }
+
+  // assertions
+  async asserErrorMessage() {
+    await expect(this.errorMessage).toContainText(
+      'Login and/or password are wrong.'
+    );
   }
 
   // Define login page methods
   async visit() {
-    await this.page.goto('http://zero.webappsecurity.com/')
+    await this.page.goto('http://zero.webappsecurity.com/');
   }
 }

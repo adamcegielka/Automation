eb3bb95eae956acfef00d1e7a1bcf8c7e2442a4a
Adam <adamcegielka@tlen.pl>
2023-05-30 15:48:33 +0200

    🚀  Adding HomePage.ts

---
+++ Playwright-Automated-Software-Testing/page-objects/HomePage.ts
@@ -0,0 +1,24 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+  readonly signInButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.signInButton = page.locator('#signin_button');
+  }
+
+  async visit() {
+    await this.page.goto('http://zero.webappsecurity.com/');
+  }
+
+  async clickOnSignIn() {
+    await this.signInButton.click();
+  }
+
+  // assertions
+  async assertHomePage() {
+    await expect(this.page).toHaveTitle('Zero - Personal Banking - Loans - Credit Cards');
+  }
+}

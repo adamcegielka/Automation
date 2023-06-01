ba43c01967c2b63a9981326d0f2c4826c578407f
Adam <adamcegielka@tlen.pl>
2023-06-01 17:20:35 +0200

    🚀  Adding a file and refactoring

---
+++ Playwright-Automated-Software-Testing/tests/e2e-tests/e2e-filter-transactions-refactor.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+import { loginRegistrationData } from '../e2e-data/e2e-login.data';
+import { HomePage } from '../../page-objects/HomePage';
+import { LoginPage } from '../../page-objects/LoginPage';
+
+test.describe('Filter transactions', () => {
+  let homePage: HomePage;
+  let loginPage: LoginPage;
+    
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    loginPage = new LoginPage(page);
+    
+    const userLogin = loginRegistrationData.userLogin;
+    const userPassword = loginRegistrationData.userPassword;
+    const goBack = 'http://zero.webappsecurity.com/index.html';
+
+    await homePage.visit();
+    await homePage.clickOnSignIn();
+    await loginPage.login(userLogin, userPassword);
+    
+    // ERROR
+    await page.goto(goBack);
+    await page.click('#account_activity_link');
+    await page.click('#transfer_funds_tab');
+  });
+
+  // selection by value: <option value="2">Checking</option>
+  test('verify the results for each accont', async ({ page }) => {
+    await page.click('#account_activity_tab');
+    await page.selectOption('#aa_accountId', '2');
+    const checkingAccount = await page.locator('#all_transactions_for_account tbody tr');
+    await expect(checkingAccount).toHaveCount(3);
+
+    await page.selectOption('#aa_accountId', '4');
+    const checkingLoan = await page.locator('#all_transactions_for_account tbody tr');
+    await expect(checkingLoan).toHaveCount(2);
+    
+    await page.selectOption('#aa_accountId', '6');
+    const noResults = await page.locator('.well');
+    await expect(noResults).toBeVisible();
+  })
+});

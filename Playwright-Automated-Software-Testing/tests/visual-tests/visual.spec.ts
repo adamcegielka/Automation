21ca26f4d0a1027dfef031f93cb24a0bbd466861
Adam <adamcegielka@tlen.pl>
2023-06-02 15:42:02 +0200

    🔧 deleting 'only'

--- Playwright-Automated-Software-Testing/tests/visual-tests/visual.spec.ts
+++ Playwright-Automated-Software-Testing/tests/visual-tests/visual.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.only('Visual Regression Testing', () => {
+test.describe('Visual Regression Testing', () => {
   test('full page snapshot', async ({ page }) => {
     await page.goto('https://www.example.com');
     expect(await page.screenshot()).toMatchSnapshot('homepage.png');

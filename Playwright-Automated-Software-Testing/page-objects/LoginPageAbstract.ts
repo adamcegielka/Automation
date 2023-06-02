173c779c334232c79eda51d1e7450745cea6128f
Adam <adamcegielka@tlen.pl>
2023-06-02 14:05:14 +0200

    🚀  Adding Abstract Page

---
+++ Playwright-Automated-Software-Testing/page-objects/LoginPageAbstract.ts
@@ -0,0 +1,45 @@
+import { expect, Locator, Page } from '@playwright/test';
+import { AbstractPage } from './abstractPage';
+
+export class LoginPageAbstract extends AbstractPage {
+  // Define selectors
+  // readonly page: Page;
+  readonly userNameInput: Locator;
+  readonly userPasswordInput: Locator;
+  readonly submitButton: Locator;
+  readonly usernameButton: Locator;
+  readonly logoutButton: Locator;
+  readonly errorMessage: Locator;
+
+  // Init selector using constructor
+  constructor(page: Page) {
+    // this.page = page;
+    super(page);
+    this.userNameInput = page.locator('#user_login');
+    this.userPasswordInput = page.locator('#user_password');
+    this.submitButton = page.locator('text=Sign in');
+
+    this.usernameButton = page.getByText('username');
+    this.logoutButton = page.getByRole('link', { name: 'Logout' });
+
+    this.errorMessage = page.locator('.alert-error');
+  }
+
+  async login(userName: string, userPassword: string) {
+    await this.userNameInput.type(userName);
+    await this.userPasswordInput.type(userPassword);
+    await this.submitButton.click();
+  }
+
+  async logout() {
+    await this.usernameButton.click();
+    await this.logoutButton.click();
+  }
+
+  // assertions
+  async asserErrorMessage() {
+    await expect(this.errorMessage).toContainText(
+      'Login and/or password are wrong.'
+    );
+  }
+}
---
+++ Playwright-Automated-Software-Testing/tests/e2e-tests/e2e-login-refactor-abstract.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+import { loginRegistrationData } from '../e2e-data/e2e-login.data';
+import { LoginPageAbstract } from '../../page-objects/LoginPageAbstract';
+import { HomePage } from '../../page-objects/HomePage';
+
+test.describe('Login and Logout Flow', () => {
+  let loginPage: LoginPageAbstract;
+  let homePage: HomePage;
+
+  // Before Hook
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPageAbstract(page);
+    homePage = new HomePage(page);
+
+    await homePage.visit();
+  });
+
+  // Negative Login
+  test('negative login', async ({ page }) => {
+    const userLoginIncorrect = loginRegistrationData.userLoginIncorrect;
+    const userPasswordIncorrect = loginRegistrationData.userPasswordIncorrect;
+    
+    await homePage.clickOnSignIn();
+    await loginPage.login(userLoginIncorrect, userPasswordIncorrect);
+    await loginPage.wait(3000);
+    await loginPage.asserErrorMessage();
+  });
+
+  // Positive Login and Logout
+  test('positive login and logout', async ({ page }) => {
+    const userLogin = loginRegistrationData.userLogin;
+    const userPassword = loginRegistrationData.userPassword;
+    const verifyActivityMessage = 'Show Transactions';
+
+    await homePage.clickOnSignIn();
+    await loginPage.login(userLogin, userPassword);
+
+    // ERROR - CLICKING THE BACK BUTTON
+    const goBack = 'http://zero.webappsecurity.com/index.html';
+    await page.goto(goBack);
+    await page.click('#account_activity_link');
+    // ...
+
+    const accountActivity = await page.locator ('.board-header');
+    await expect(accountActivity).toContainText(verifyActivityMessage);
+
+    await loginPage.logout();
+
+    await homePage.assertHomePage();
+  });
+});

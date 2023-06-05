adbdc75e366bffdeb91060d301d825568c470b22
Adam <adamcegielka@tlen.pl>
2023-06-05 15:23:16 +0200

    🚀  Adding api test - Patch

---
+++ Playwright-Automated-Software-Testing/tests/api/apiPatch.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('API Testing - PATCH', () => {
+  const baseUrl = 'https://reqres.in/api';
+
+  test.only('PATCH - update user', async ({ request }) => {
+    const respons = await request.patch(`${baseUrl}/users/2`, {
+      data: {
+        name: 'morpheus',
+        job: 'zion resident',
+      },
+    });
+    const responsBody = JSON.parse(await respons.text());
+
+    expect(respons.status()).toBe(200);
+    expect(responsBody.name).toBe('morpheus');
+    expect(responsBody.job).toBe('zion resident');
+    expect(responsBody.updatedAt).toBeTruthy();
+    console.log(responsBody);
+  });
+});

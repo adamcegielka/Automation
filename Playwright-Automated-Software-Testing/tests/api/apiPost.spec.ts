4c5b078cdef02ae583c41f6aea759c0c38b57569
Adam <adamcegielka@tlen.pl>
2023-06-05 13:41:06 +0200

    🚀  Adding api test - POST

---
+++ Playwright-Automated-Software-Testing/tests/api/apiPost.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.parallel('API Test - POST', () => {
+  const baseUrl = 'https://reqres.in/api';
+
+  test('create new user 222', async ({ request }) => {
+    const respons = await request.post(`${baseUrl}/users`, {
+      data: {
+        id: 222
+      },
+    });
+    const responsBody = JSON.parse(await respons.text());
+    expect(respons.status()).toBe(201);
+    console.log(responsBody);
+  });
+
+  test('create new user 333', async ({ request }) => {
+    const respons = await request.post(`${baseUrl}/users`, {
+      data: {
+        id: 333
+      },
+    });
+    const responsBody = JSON.parse(await respons.text());
+    expect(responsBody.id).toBe(333);
+    expect(responsBody.createdAt).toBeTruthy();
+  });
+});

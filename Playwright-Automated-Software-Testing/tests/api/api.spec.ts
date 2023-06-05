eb29ca0beb5d0fe277755d57bd2f87d40967ca1b
Adam <adamcegielka@tlen.pl>
2023-06-05 12:15:59 +0200

    🔧 adding Simple API Test

--- Playwright-Automated-Software-Testing/tests/api/api.spec.ts
+++ Playwright-Automated-Software-Testing/tests/api/api.spec.ts
@@ -3,8 +3,14 @@ import { test, expect } from '@playwright/test';
 test.describe.parallel('API Testing', () => {
   const baseUrl = 'https://reqres.in/api';
 
-  test('Simple API Test - Assert Respons Status', async ({ request }) => {
+  test('Simple API Test: Assert Respons Status', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/2`);
     expect(response.status()).toBe(200);
   });
+
+  test('Simple API Test: Assert Invalid Endpoint', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/users/non-existing-endpoint`);
+    expect(response.status()).toBe(404);
+
+  })
 });

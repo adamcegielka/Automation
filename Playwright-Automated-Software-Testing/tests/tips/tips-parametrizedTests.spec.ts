c5339c7079bcb1bdf316c0311bc7687979c2131d
Adam <adamcegielka@tlen.pl>
2023-06-07 10:38:39 +0200

    🚀  Adding test paramertized tests

---
+++ Playwright-Automated-Software-Testing/tests/tips/tips-parametrizedTests.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+
+test.describe('Tips & Tricks Section', () => {
+
+  // PARAMETRIZED TESTS
+  const people = ['Adam', 'Ewa', 'Jan', 'Ola', 'Darek'];
+  for (const name of people) {
+    test.only(`running test for ${name}`, async ({ page }) => {
+        await page.goto('http://zero.webappsecurity.com/');
+        await page.type('#searchTerm', `${name}`);
+        await page.waitForTimeout(3000);
+    });
+  }
+});
+
+// npx playwright test --config=playwright.config.ts --project=Chromium --headed

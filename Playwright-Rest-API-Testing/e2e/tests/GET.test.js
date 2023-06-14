580b3856ca5aaddae100ca8740bbaa7d3998e9e7
Adam <adamcegielka@tlen.pl>
2023-06-14 17:36:13 +0200

    🔧 Adding test

--- Playwright-Rest-API-Testing/e2e/tests/GET.test.js
+++ Playwright-Rest-API-Testing/e2e/tests/GET.test.js
@@ -4,4 +4,18 @@ test('retrieve user posts', async ({ request }) => {
     const response = await request.get('/posts')
     expect(response.ok()).toBeTruthy()
     expect(response.status()).toBe(200)
+    console.log(await response.json());
 })
+
+test('retrieve user post', async ({ request }) => {
+    const response = await request.get('/posts/1')
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+    expect(await response.json()).toEqual(expect.objectContaining({
+        "id": 1,
+        "userId": 1
+    }))
+    console.log(await response.json());
+})
+
+// https://jsonplaceholder.typicode.com/

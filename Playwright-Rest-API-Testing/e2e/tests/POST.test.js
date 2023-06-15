24020a4fd273efdd2d0747a9219c4e59b7d65708
Adam <adamcegielka@tlen.pl>
2023-06-15 09:23:00 +0200

    🚀  creating POST.test.js

---
+++ Playwright-Rest-API-Testing/e2e/tests/POST.test.js
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test"
+
+test('create user post', async ({ request }) => {
+    const response = await request.post('/posts', {
+        data: {
+            title: 'New Post',
+            body: 'This is a new post',
+            userId: 1
+        }
+    })
+    expect(response.status()).toBe(201)
+    expect(await response.json()).toEqual((expect.objectContaining({
+        "body": "This is a new post",
+        "id": 101,
+        "title": "New Post",
+        "userId": 1
+    })))
+    console.log(await response.json())
+})
+
+// https://jsonplaceholder.typicode.com/

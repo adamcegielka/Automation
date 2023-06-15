f79ca1c6c577a66e0207f51c6569615b16119b44
Adam <adamcegielka@tlen.pl>
2023-06-15 19:15:25 +0200

    🔧 reinstallation

--- Playwright-BDD-CodeceptJS/example_test.ts
+++
@@ -1,7 +0,0 @@
-Feature('Example test')
-
-Scenario('Should load example.com webside', ({ I }) => {
-  I.amOnPage('http://www.example.com');
-  // I.saveScreenshot('frontpageScreenshot.png');
-  I.see('Example');
-})
--- Playwright-BDD-CodeceptJS/tests/homepage_test.ts
+++ Playwright-BDD-CodeceptJS/tests/homepage_test.ts
@@ -1,6 +1,6 @@
-Feature('Homepage');
+// Feature('Homepage');
 
-Scenario('Verify page elements',  ({ I }) => {
-    I.amOnPage('http://demo.guru99.com/v4/');
-    I.see('Guru99 Bank');
-});
+// Scenario('Verify page elements',  ({ I }) => {
+//     I.amOnPage('http://demo.guru99.com/v4/');
+//     I.see('Guru99 Bank');
+// });

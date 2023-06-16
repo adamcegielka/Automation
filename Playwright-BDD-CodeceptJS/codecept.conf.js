437f37ecdc446ad97e48a708fb4d115c4a211fa9
Adam <adamcegielka@tlen.pl>
2023-06-16 09:53:32 +0200

    🔧 update

--- Playwright-BDD-CodeceptJS/codecept.conf.js
+++ Playwright-BDD-CodeceptJS/codecept.conf.js
@@ -20,5 +20,19 @@ exports.config = {
   include: {
     I: './steps_file.js'
   },
-  name: 'Playwright-BDD-CodeceptJS'
+  bootstrap: null,
+  mocha: {},
+  name: 'Playwright-BDD-CodeceptJS',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
 }

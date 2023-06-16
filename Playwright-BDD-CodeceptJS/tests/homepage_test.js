c95ab2b65d955e4750f60856911e4eb113df8eca
Adam <adamcegielka@tlen.pl>
2023-06-16 09:00:38 +0200

    🚀  creating homepage test

--- Playwright-BDD-CodeceptJS/tests/homepage_test.js
+++ Playwright-BDD-CodeceptJS/tests/homepage_test.js
@@ -3,4 +3,8 @@ Feature('Homepage');
 Scenario('Verify page elements',  ({ I }) => {
     I.amOnPage('http://demo.guru99.com/v4/');
     I.see('Guru99 Bank');
+    I.dontSee('Google');
+    I.seeElement('h4');
+    I.dontSeeElement('h1');
+    I.dontSeeElement('#dontExist');
 });

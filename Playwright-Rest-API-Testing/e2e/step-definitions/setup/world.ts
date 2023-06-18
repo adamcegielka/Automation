b1c7dd99e6ba8121668066fa0d3ba7078a3520df
Adam <adamcegielka@tlen.pl>
2023-06-18 14:42:06 +0200

    🔧 update

--- Playwright-Rest-API-Testing/e2e/step-definitions/setup/world.ts
+++ Playwright-Rest-API-Testing/e2e/step-definitions/setup/world.ts
@@ -1,5 +1,6 @@
 import playwright, { APIRequestContext } from 'playwright';
 import { World, IWorldOptions, setWorldConstructor } from '@cucumber/cucumber';
+import { GlobalAPIResponseVariables } from '../../env/global';
 
 export type Api = {
   request: APIRequestContext;
@@ -8,7 +9,11 @@ export type Api = {
 export class ScenarioWorld extends World {
   constructor(options: IWorldOptions) {
     super(options);
+
+    this.globalAPIResponseVariables = {};
   }
+
+  globalAPIResponseVariables: GlobalAPIResponseVariables;
 
   api!: Api;
 

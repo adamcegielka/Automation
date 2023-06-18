ee641b36d8d82d170ff7b12de371872ac55b2e30
Adam <adamcegielka@tlen.pl>
2023-06-18 14:57:02 +0200

    🔧 correction of an error

--- Playwright-Rest-API-Testing/e2e/step-definitions/assertions/response-steps.ts
+++ Playwright-Rest-API-Testing/e2e/step-definitions/assertions/response-steps.ts
@@ -2,7 +2,7 @@ import { Then } from '@cucumber/cucumber';
 import { ScenarioWorld } from '../setup/world';
 
 Then(
-  /^the response was (successful)?(unsuccessful)?$/,
+  /^The response was (successful)?(unsuccessful)?$/,
   async function (
     this: ScenarioWorld,
     successful: boolean,

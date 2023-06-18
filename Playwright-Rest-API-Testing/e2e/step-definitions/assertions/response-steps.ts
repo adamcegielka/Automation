dd94f727229b7129a6ad0879b36570ecda32b93e
Adam <adamcegielka@tlen.pl>
2023-06-18 17:20:12 +0200

    🔧 update test

--- Playwright-Rest-API-Testing/e2e/step-definitions/assertions/response-steps.ts
+++ Playwright-Rest-API-Testing/e2e/step-definitions/assertions/response-steps.ts
@@ -1,5 +1,6 @@
 import { Then } from '@cucumber/cucumber';
 import { ScenarioWorld } from '../setup/world';
+import { expect } from '@playwright/test';
 
 Then(
   /^The response was (successful)?(unsuccessful)?$/,
@@ -11,5 +12,24 @@ Then(
     const { globalAPIResponseVariables } = this;
 
     console.log(await globalAPIResponseVariables.response.text());
+  }
+);
+
+Then(
+  /^The second response is (successful)?(unsuccessful)?$/,
+  async function (this: ScenarioWorld, successful: boolean, unsuccessful: boolean) {
+    const {
+        globalAPIResponseVariables
+    } = this;
+
+    console.log(`The second response is ${unsuccessful? 'unsuccessful ' : 'successful '}`);
+
+    const response = globalAPIResponseVariables.response;
+
+    if (unsuccessful) {
+      expect(response.ok()).toBeFalsy();
+    } else {
+      expect(response.ok()).toBeTruthy();
+    }
   }
 );

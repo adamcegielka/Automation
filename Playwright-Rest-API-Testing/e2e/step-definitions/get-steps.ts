f0a4657a7bb9088cee95ea3b627d029bc2b2c7a7
Adam <adamcegielka@tlen.pl>
2023-06-18 14:19:40 +0200

    🔧 update

--- Playwright-Rest-API-Testing/e2e/step-definitions/get-steps.ts
+++ Playwright-Rest-API-Testing/e2e/step-definitions/get-steps.ts
@@ -1,8 +1,15 @@
 import { Given } from "@cucumber/cucumber";
+import { ScenarioWorld } from "./setup/world";
 
 Given(
     /^I retrieve "([^"]*)"$/,
-    async function(route: string) {
+    async function(this: ScenarioWorld, route: string) {
+        const {
+            api: { request },
+        } = this
         
+        const response = await request.get('https://jsonplaceholder.typicode.com/'+route)
+
+        console.log(await response.text())
     }
 )

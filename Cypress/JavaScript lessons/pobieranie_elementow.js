e38822b68bdfc8bfeee745186310c803d02eaf1c
Adam <adamcegielka@tlen.pl>
2023-07-24 17:03:25 +0200

    feat: add `pobieranie_elementow.js` file

---
+++ Cypress/JavaScript lessons/pobieranie_elementow.js
@@ -0,0 +1,13 @@
+// Pobieranie elementów ze strony
+
+const searchId = document.getElementById('search_query_top')
+
+const searchClass = document.getElementsByClassName('search_query')
+// const searchClass = document.querySelector('#search_query')
+
+const searchTagName_a = document.getElementsByTagName('a')
+const searchTagName1_a = document.querySelectorAll('a')
+
+const searchTagName_name = document.getElementsByTagName('email')
+
+const searchImg = document.querySelector('div#htmlcontent_top li:nth-child(1) img')

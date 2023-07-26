e6e6c9837dee598eb2f9a09702b33906af9405ed
Adam <adamcegielka@tlen.pl>
2023-07-26 19:59:10 +0200

    fix: coda repair by `Prettier`

--- Cypress/JavaScript lessons/tablice-metody.js
+++ Cypress/JavaScript lessons/tablice-metody.js
@@ -7,42 +7,42 @@ console.log(names.length); // 4
 console.log(names.length > 8); // false
 
 // określony element tablicy
-console.log(names);    // (4) ['Adam', 'Ewa', 'Tom', 'Anna']
+console.log(names);    // (4)['Adam', 'Ewa', 'Tom', 'Anna']
 console.log(names[3]); // Anna
 
 // push() - dodanie elementu na końcu
 console.log(names.push('Ola')); // 5
-console.log(names);             // (5) ['Adam', 'Ewa', 'Tom', 'Anna', 'Ola']
+console.log(names);             // (5)['Adam', 'Ewa', 'Tom', 'Anna', 'Ola']
 
 // nadpisanie
 console.log(names[4] = 'Jan');  // Jan
-console.log(names);             // (5) ['Adam', 'Ewa', 'Tom', 'Anna', 'Jan']
+console.log(names);             // (5)['Adam', 'Ewa', 'Tom', 'Anna', 'Jan']
 
 // pop() - usuwanie ostatniego elementu
 console.log(names.pop());       // Jan
-console.log(names);             // (4) ['Adam', 'Ewa', 'Tom', 'Anna']
+console.log(names);             // (4)['Adam', 'Ewa', 'Tom', 'Anna']
 
 // shift() - usuwanie pirwszego elementu
 console.log(names.shift());     // Adam
-console.log(names);             // (3) ['Ewa', 'Tom', 'Anna']
+console.log(names);             // (3)['Ewa', 'Tom', 'Anna']
 
 // unshift() - dodawanie elementu na początku
 console.log(names.unshift('Dawid')); // 4
-console.log(names);                  // (4) ['Dawid', 'Ewa', 'Tom', 'Anna']
+console.log(names);                  // (4)['Dawid', 'Ewa', 'Tom', 'Anna']
 
 // splice() - usunięcie od wybranego elementu pozostałych elementów
 console.log(names.indexOf('Tom'));     // 2
-console.log(names.splice(2));          // (2) ['Tom', 'Anna']
-console.log(names);                    // (2) ['Dawid', 'Ewa']
+console.log(names.splice(2));          // (2)['Tom', 'Anna']
+console.log(names);                    // (2)['Dawid', 'Ewa']
 
 console.log(names.indexOf('Monika'));  // -1
 console.log(names);
 
 // splice() - usunięcie wybranego elementu
 console.log(names.push('Darek', 'Grzegorz')); // 4
-console.log(names);                           // (4) ['Dawid', 'Ewa', 'Darek', 'Grzegorz']
+console.log(names);                           // (4)['Dawid', 'Ewa', 'Darek', 'Grzegorz']
 console.log(names.splice(2, 1));              // ['Darek']
-console.log(names);                           // (3) ['Dawid', 'Ewa', 'Grzegorz']
+console.log(names);                           // (3)['Dawid', 'Ewa', 'Grzegorz']
 
 // join() - łączenie do stringa
 console.log(names.join(' '));         // 'Dawid Ewa Grzegorz'
@@ -50,13 +50,13 @@ console.log(typeof(names.join(' '))); // string
 console.log(names.join(' | '));       // 'Dawid | Ewa | Grzegorz'
 
 // reverse() - odwucenie elementów
-console.log(names.reverse()); // (3) ['Grzegorz', 'Ewa', 'Dawid']
+console.log(names.reverse()); // (3)['Grzegorz', 'Ewa', 'Dawid']
 
 // sort() - sortowanie
 console.log(names.push('Adam', 'Franc'));   // 5
-console.log(names);                         // (5) ['Dawid', 'Ewa', 'Grzegorz', 'Adam', 'Franc']
-console.log(names.sort());                  // (5) ['Adam', 'Dawid', 'Ewa', 'Franc', 'Grzegorz']
-console.log(names.sort().reverse());        // (5) ['Grzegorz', 'Franc', 'Ewa', 'Dawid', 'Adam']
+console.log(names);                         // (5)['Dawid', 'Ewa', 'Grzegorz', 'Adam', 'Franc']
+console.log(names.sort());                  // (5)['Adam', 'Dawid', 'Ewa', 'Franc', 'Grzegorz']
+console.log(names.sort().reverse());        // (5)['Grzegorz', 'Franc', 'Ewa', 'Dawid', 'Adam']
 
 // map() - mapowanie elementów
 names.map(item => {
@@ -107,4 +107,4 @@ const cars = ['Audi', 'BMW', 'Opel'];
 const numbers = [3, 5 ,6];
 
 const list = cars.concat(numbers);
-console.log(list);  // (6) ['Audi', 'BMW', 'Opel', 3, 5, 6]
+console.log(list);  // (6)['Audi', 'BMW', 'Opel', 3, 5, 6]

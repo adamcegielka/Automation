bb049e41fb9c7dbd9d75aff5ac65b1304bad5060
Adam Cegiełka <adamcegielka@tlen.pl>
2023-10-31 20:15:05 +0100

    tests: change setings

--- Playwright-Automated-Software-Testing/playwright.config.ts
+++ Playwright-Automated-Software-Testing/playwright.config.ts
@@ -4,14 +4,14 @@ import { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
     timeout: 60000,
-    testDir: 'tests/tips',
+    testDir: 'tests/',
     retries: 0,
     use: {
         headless: true,
         viewport: { width: 1280, height: 720 },
         actionTimeout: 15000,
         ignoreHTTPSErrors: true,
-        video: 'retain-on-failure',
+        video: 'on',
         screenshot: 'only-on-failure',
     },
     projects: [
